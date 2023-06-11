ec64051adeaffb117ec80e7cddebbe847977faf6
hammadkhanwazir <insafstudentpti@gmail.com>
2023-06-12 02:09:47 +0500

    last commit

--- src/script.js
+++ src/script.js
@@ -37,18 +37,38 @@ window.addEventListener("scroll", () => {
     scrollButton.classList.remove("show");
   }
 });
-document.addEventListener("DOMContentLoaded", function () {
-  const textContainer = document.getElementById("animated-text");
-  const text = textContainer.innerText;
-  let animatedText = "";
+// document.addEventListener("DOMContentLoaded", function () {
+//   const textContainer = document.getElementById("animated-text");
+//   const text = textContainer.innerText;
+//   let animatedText = "";
 
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === " ") {
-      animatedText += " ";
-    } else {
-      animatedText += `<span class="animate-letter">${text[i]}</span>`;
-    }
-  }
+//   for (let i = 0; i < text.length; i++) {
+//     if (text[i] === " ") {
+//       animatedText += " ";
+//     } else {
+//       animatedText += `<span class="animate-letter">${text[i]}</span>`;
+//     }
+//   }
 
-  textContainer.innerHTML = animatedText;
-});
+//   textContainer.innerHTML = animatedText;
+// });
+
+// // Get all the slides
+// const slides = document.querySelectorAll(".slide");
+
+// let currentIndex = 0;
+// const slideInterval = setInterval(showNextSlide, 2000); // Change slide every 2 seconds
+
+// function showNextSlide() {
+//   // Hide current slide
+//   slides[currentIndex].style.opacity = 0;
+
+//   // Increment the index
+//   currentIndex = (currentIndex + 1) % slides.length;
+
+//   // Show next slide
+//   slides[currentIndex].style.opacity = 1;
+// }
+
+// animation javascript
+// Select the slide container
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Red Hat Display",
+          "Poppins",
+          "Roboto",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
   },
   plugins: [],
 };
